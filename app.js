f2d43fbb3ff41bfd8184978d2a755ab8541fb898
sylviawanjiku <sylviakatherine22@gmail.com>
2021-04-12 11:46:26 +0300

    fix errors

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/',(req,res) => {
+app.get('/', (req, res) => {
 	res.send('Hello hacker with gorgeous eyes 😊');
 });
-app.listen(port,()=> console.log(`She hacked on port ${port}!🤓`))
+app.get('/dem', (req, res) => {
+	res.set('X-full-stack', '4life');
+	res.status(418);
+	res.send('I AM AWESOME')
+})
+app.listen(port, () => console.log(`She hacked on port ${port}!🤓`))
